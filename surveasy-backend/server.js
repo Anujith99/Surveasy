894b58be0c6f25cb7406b11fc855a1a5ee88283f
Anujith99 <banuj99@gmail.com>
2021-06-28 21:18:01 +0530

    Added logger and logger middleware

--- surveasy-backend/server.js
+++ surveasy-backend/server.js
@@ -1,12 +1,18 @@
 import express from "express";
 import cors from "cors";
 
+import loggerMiddleware from "./middlewares/loggerMiddleware.js";
+
+import logger from "./config/logger.js";
+
 const app = express();
 
 app.use(
   cors({ origin: process.env.ALLOWED_ORIGIN, optionsSuccessStatus: 200 })
 );
 app.use(express.json());
+
+app.use(loggerMiddleware);
 
 app.get("/", (req, res) => {
   res.json({ message: "This is the base route" });
@@ -16,8 +22,8 @@ let port = process.env.port || 5000;
 
 app.listen(port, (err) => {
   if (err) {
-    console.log("Error when starting up server");
+    logger.error("Error when starting up server");
   } else {
-    console.log(`Server listening on port ${port}`);
+    logger.info(`Server listening on port ${port}`);
   }
 });
