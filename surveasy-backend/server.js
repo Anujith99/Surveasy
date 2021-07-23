fcef896492bcee6622487a04aaed58711a1a4151
Anujith99 <banuj99@gmail.com>
2021-07-23 21:41:08 +0530

    port change

--- surveasy-backend/server.js
+++ surveasy-backend/server.js
@@ -40,7 +40,7 @@ app.use("/dashboard", dashboardRoutes);
 app.use(notFoundMiddleware);
 app.use(errorHandlingMiddleware);
 
-let port = process.env.PORT || 5000;
+let port = process.env.PORT || 8000;
 
 app.listen(port, (err) => {
   if (err) {
